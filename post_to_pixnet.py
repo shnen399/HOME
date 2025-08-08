d598ae2be902ee13079e4b1356fc83ae4e1abc49
shnen399 <t0970313177@gmail.com>
2025-08-08 20:27:41 +0800

    Add files via upload

--- post_to_pixnet.py
+++ post_to_pixnet.py
@@ -1,4 +1,5 @@
 def post_to_pixnet(email, password, article, keywords):
-    print(f"模擬登入 {email} 並發文中...")
-    # 模擬延遲與回傳
-    return True, "https://pixnet.net/blog/post/123456789"
+    username = email.split("@")[0]
+    fake_post_id = "123456789"
+    blog_link = f"https://{username}.pixnet.net/blog/post/{fake_post_id}"
+    return True, blog_link
