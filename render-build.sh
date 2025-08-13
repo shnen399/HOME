65c5499abf6ffeeb15971098ab8c4356a68bc9bb
shnen399 <t0970313177@gmail.com>
2025-08-13 12:33:48 +0800

    render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
+
 # 升級 pip
 pip install --upgrade pip
 
 # 安裝 Python 套件
 pip install -r requirements.txt
 
-# 安裝 Playwright Chromium 瀏覽器
-npx playwright install chromium
+# 安裝 Playwright Chromium 到指定快取路徑
+export PLAYWRIGHT_BROWSERS_PATH=/opt/render/.cache/ms-playwright
+python -m playwright install chromium
