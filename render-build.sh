3bef3600e0b4a0c6b7661ad6fac07cad4f72570a
shnen399 <t0970313177@gmail.com>
2025-08-13 17:06:26 +0800

    render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -8,7 +8,8 @@ pip install -r requirements.txt
 # 設定快取路徑（跟環境變數一致）
 export PLAYWRIGHT_BROWSERS_PATH=/opt/render/.cache/ms-playwright
 
-# 安裝 Chromium 與依賴
+# 安裝 Playwright 與 Chromium 及依賴
+npm install -g playwright
 npx --yes playwright install chromium --with-deps
 
 # 開放快取讀取權限
